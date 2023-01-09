ec83b95672853e57ed73fd2711dffb9e3e8d75b4
MSA 3837 <MatheusAlbertoClash@gmail.com>
2023-01-09 13:32:02 -0300

    Levels

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -1,18 +1,22 @@
-#include "Game.h"
+#include "game.h"
 
 #include "ResourceManager.h"
 #include "SpriteRenderer.h"
+#include "GameObject.h"
 
 SpriteRenderer* Renderer;
 
+GameObject* Player;
+
 Game::Game(unsigned int width, unsigned int height)
-	: State(GAME_ACTIVE), Keys(), Width(width), Height(height)
+    : State(GAME_ACTIVE), Keys(), Width(width), Height(height)
 {
 }
 
 Game::~Game()
 {
     delete Renderer;
+    delete Player;
 }
 
 void Game::Init()
@@ -25,25 +29,73 @@ void Game::Init()
     ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
     ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
 
-    Shader spriteRenderer = ResourceManager::GetShader("sprite");
-    
     Shader shader = ResourceManager::GetShader("sprite");
 
     Renderer = new SpriteRenderer(shader);
 
+    ResourceManager::LoadTexture("Source/Sprites/background.jpg", false, "background");
     ResourceManager::LoadTexture("Source/Sprites/awesomeface.png", true, "face");
-}
+    ResourceManager::LoadTexture("Source/Sprites/block.png", false, "block");
+    ResourceManager::LoadTexture("Source/Sprites/block_solid.png", false, "block_solid");
+    ResourceManager::LoadTexture("Source/Sprites/paddle.png", true, "paddle");
 
-void Game::ProcessInput(float dt)
-{
+    GameLevel one; 
+    one.Load("Source/Levels/Standard.lvl", this->Width, this->Height / 2);
+
+    GameLevel two; 
+    two.Load("Source/Levels/FewSmallGaps.lvl", this->Width, this->Height / 2);
+
+    GameLevel three; 
+    three.Load("Source/Levels/SpaceInvaders.lvl", this->Width, this->Height / 2);
+
+    GameLevel four; 
+    four.Load("Source/Levels/BounceGalore.lvl", this->Width, this->Height / 2);
+
+    this->Levels.push_back(one);
+    this->Levels.push_back(two);
+    this->Levels.push_back(three);
+    this->Levels.push_back(four);
+
+    this->Level = 1;
+
+    glm::vec2 playerPos = glm::vec2(this->Width / 2.0f - PLAYER_SIZE.x / 2.0f, this->Height - PLAYER_SIZE.y);
+
+    Player = new GameObject(playerPos, PLAYER_SIZE, ResourceManager::GetTexture("paddle"));
 }
 
 void Game::Update(float dt)
 {
 }
 
+void Game::ProcessInput(float dt)
+{
+    if (this->State == GAME_ACTIVE)
+    {
+        float velocity = PLAYER_VELOCITY * dt;
+
+        if (this->Keys[GLFW_KEY_A])
+        {
+            if (Player->Position.x >= 0.0f)
+                Player->Position.x -= velocity;
+        }
+        if (this->Keys[GLFW_KEY_D])
+        {
+            if (Player->Position.x <= this->Width - Player->Size.x)
+                Player->Position.x += velocity;
+        }
+    }
+}
+
 void Game::Render()
 {
-    Texture2D texture = ResourceManager::GetTexture("face");
-    Renderer->DrawSprite(texture, glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+    if (this->State == GAME_ACTIVE)
+    {
+        Texture2D background = ResourceManager::GetTexture("background");
+
+        Renderer->DrawSprite(background, glm::vec2(0.0f, 0.0f), glm::vec2(this->Width, this->Height), 0.0f);
+
+        this->Levels[this->Level].Draw(*Renderer);
+
+        Player->Draw(*Renderer);
+    }
 }
--- Source/Game.h
+++ Source/Game.h
@@ -1,27 +1,39 @@
 #pragma once
+#include <GL/glew.h>
 
+#include <GLFW/glfw3.h>
 
-enum GameState
+#include "GameLevel.h"
+
+enum GameState 
 {
-	GAME_MENU,
-	GAME_ACTIVE,
-	GAME_WIN
+    GAME_ACTIVE,
+    GAME_MENU,
+    GAME_WIN
 };
+
+const glm::vec2 PLAYER_SIZE(100.0f, 20.0f);
+
+const float PLAYER_VELOCITY(500.0f);
 
 class Game
 {
 public:
-	GameState State;
+    GameState State;
 
-	bool Keys[1024];
-	unsigned int Width, Height;
+    bool Keys[1024];
 
-	Game(unsigned int width, unsigned int height);
-	~Game();
+    unsigned int Width, Height;
+    std::vector<GameLevel> Levels;
 
-	void Init();
+    unsigned int Level;
 
-	void ProcessInput(float dt);
-	void Update(float dt);
-	void Render();
+    Game(unsigned int width, unsigned int height);
+    ~Game();
+
+    void Init();
+
+    void ProcessInput(float dt);
+    void Update(float dt);
+    void Render();
 };
